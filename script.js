707b9ed5c32fb97a49016bb7ae4b6644d18646a5
BaratBence <47627385+BaratBence@users.noreply.github.com>
2026-03-29 23:19:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+fetch('images.json')
+  .then(res => res.json())
+  .then(images => {
+    const gallery = document.getElementById('gallery');
 
+    images.forEach(img => {
+      const el = document.createElement('img');
+      el.src = 'images/' + img;
+
+      el.addEventListener('click', () => {
+        openLightbox(el.src);
+      });
+
+      gallery.appendChild(el);
+    });
+  });
+
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightbox-img');
+const closeBtn = document.getElementById('close');
+
+function openLightbox(src) {
+  lightbox.style.display = 'flex';
+  lightboxImg.src = src;
+}
+
+closeBtn.onclick = () => lightbox.style.display = 'none';
+lightbox.onclick = (e) => {
+  if (e.target === lightbox) {
+    lightbox.style.display = 'none';
+  }
+};
